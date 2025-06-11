301346b5e808de75ce583f5d22d7b1da602b9277
nickjgastis <nickjgastis@gmail.com>
2025-06-11 13:49:04 -0700

    homepage pricing cards fixd and new graphics

--- src/pages/ProductPage.js
+++ src/pages/ProductPage.js
@@ -144,7 +144,13 @@ const ProductPage = () => {
                     </div>
                     <div className="enterprise-section">
                         <h3>Looking to sign up your whole clinic staff, or multiple clinics?</h3>
-                        <p>We've got you covered! Contact <a href="mailto:support@petwise.vet">support@petwise.vet</a></p>
+                        <p>We've got you covered! Contact support@petwise.vet for enterprise plans.</p>
+                        <a href="mailto:support@petwise.vet" className="enterprise-contact-btn">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+                                <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1H2zm13 2.383-4.708 2.825L15 11.105V5.383zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741zM1 11.105l4.708-2.897L1 5.383v5.722z" />
+                            </svg>
+                            Contact Us!
+                        </a>
                     </div>
                 </section>
             </div>
