d2f6a658bca92c549d5d593b11df6d7949c2fc9f
nickjgastis <nickjgastis@gmail.com>
2025-08-19 21:18:35 -0700

    Add signup route for direct Auth0 signup from marketing site

--- src/components/AutoLogin.js
+++ src/components/AutoLogin.js
@@ -1,12 +1,16 @@
 import { useEffect } from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
 
-const AutoLogin = () => {
+const AutoLogin = ({ mode = 'login' }) => {
     const { loginWithRedirect } = useAuth0();
 
     useEffect(() => {
-        loginWithRedirect();
-    }, [loginWithRedirect]);
+        loginWithRedirect({
+            authorizationParams: {
+                screen_hint: mode === 'signup' ? 'signup' : undefined
+            }
+        });
+    }, [loginWithRedirect, mode]);
 
     return null;
 };
--- src/routes.js
+++ src/routes.js
@@ -59,6 +59,7 @@ const AppRoutes = () => {
                 }
             />
             <Route path="/login" element={<AutoLogin />} />
+            <Route path="/signup" element={<AutoLogin mode="signup" />} />
             <Route path="/callback" element={null} />
             <Route path="/about" element={<AboutPage />} />
             <Route path="/blog" element={<Blog />} />
